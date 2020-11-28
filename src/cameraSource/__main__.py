04e9891f7caf86a7cebcad11428a7636a26899e8
Vladimir Varga <Vladimir.Varga@quest.com>
2020-11-28 15:26:31 +0100

    Squashed commit of the following:
    commit f3824ee7836270c84beaf5860c70ea7908b9471a
    Author: Vladimir Varga <Vladimir.Varga@quest.com>
    Date:   Sat Nov 28 15:24:43 2020 +0100
    
        finetuning
    
    commit 4e691f1ffe7a955a48fb05d09db3cc7e34449f5c
    Author: vladis <spider19@pobox.sk>
    Date:   Fri Nov 13 00:07:43 2020 +0100
    
        add zmq
    
    commit b78e563dd20fc062a82aaa91db8b0e8525951ee4
    Author: vladis <spider19@pobox.sk>
    Date:   Thu Nov 12 22:57:44 2020 +0100
    
        add debug information
    
    commit 3c9b172808b8bd362f883e32e6ea817acc3658ae
    Author: Vladimir Varga <Vladimir.Varga@quest.com>
    Date:   Wed Nov 11 12:58:58 2020 +0100
    
        fixed issues with code
    
    commit 807779824e40acfd7932d72f34a3ff7ec2541225
    Author: Vladimir Varga <Vladimir.Varga@quest.com>
    Date:   Wed Nov 11 06:06:50 2020 +0100
    
        added redis
    
    commit d20472fd5c071db05e2808c54091b1f70c93ab3c
    Author: Vladimir Varga <Vladimir.Varga@quest.com>
    Date:   Tue Nov 10 10:36:51 2020 +0100
    
        add argparse

--- src/cameraSource/__main__.py
+++ src/cameraSource/__main__.py
@@ -1,22 +1,51 @@
-import numpy as np
+import time
+from datetime import datetime
+
 import cv2
+import argparse
 
-url = "192.168.1.168:80"
+from redisclient import RedisClient
 
-capture = cv2.VideoCapture(url)
+parser = argparse.ArgumentParser(description="Parse video input to separate frames.")
+parser.add_argument('-i', '--input', metavar='http://my.video.source.com/', type=str, default='0',
+                    help="Source can be id of connected web camera or stream URL. Default '0' (internal web camera id).")
+parser.add_argument('-f', '--fps', metavar='N', type=int, default=1,
+                    help="Framerate per second. Default 1.")
+parser.add_argument('-r', '--redis-server', metavar='server:port', type=str, default='redis:6379',
+                    help="URL to redis server. Default 'redis:6379'.")
+parser.add_argument('-s', '--session', metavar='session-name', type=str, default='default',
+                    help="Session identification. Frames will be stored in redis under session-name:key. Default 'default'.")
+parser.add_argument('-d', '--debug', action='store_true', help="Write debug messages to console.")
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+args = parser.parse_args()
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+print("Arguments:")
+print(args, flush=True)
 
-    # Display the resulting frame
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+def debug(text):
+    if args.debug:
+        print("{}: {}".format(datetime.utcnow(), text), flush=True)
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+    capture = cv2.VideoCapture(args.input)
+    debug("Successfully connected to Camera '{}'.".format(args.input))
+
+    redis = RedisClient(args.redis_server)
+    debug("Successfully connected to Redis '{}'.".format(args.redis_server))
+
+    frameId = redis.getLastFrameId()
+
+    try:
+        while capture.isOpened():
+            ret, frame = capture.read()
+            debug("Got frame.")
+            frameBytes = cv2.imencode(".jpg", frame)[1].tostring()
+            redis.addFrame(frameId, frameBytes)
+            debug("New frame added to Redis. Frame id:{}".format(frameId))
+
+            frameId = frameId + 1
+            time.sleep(1/args.fps)
+    finally:
+        capture.release()
+        redis.close()
+        debug("Exiting...")
